12dd1cb2a755543a9a121810d11fb6f73410bfa9
stevemath <steve@theincognitogroup.com>
2020-11-13 14:31:40 -0500

    gps proj test

--- js/aframe-components-nft.js
+++ js/aframe-components-nft.js
@@ -19,7 +19,22 @@
         }
       });
       
-    
+      AFRAME.registerComponent('rotation-reader', {
+        /**
+         * We use IIFE (immediately-invoked function expression) to only allocate one
+         * vector or euler and not re-create on every tick to save memory.
+         */
+        tick: (function () {
+          var position = new THREE.Vector3();
+          var quaternion = new THREE.Quaternion();
+      
+          return function () {
+            this.el.object3D.getWorldPosition(position);
+            this.el.object3D.getWorldQuaternion(quaternion);
+            // position and rotation now contain vector and quaternion in world space.
+          };
+        })
+      });
 
       AFRAME.registerComponent('hotspot-click', {
    
